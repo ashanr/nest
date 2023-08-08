8301851b6316c8fa48227f55bad8b3624e8357d7
Priyankara R <priyankarar@opusxenta.com>
2023-08-08 17:37:52 +0530

    added user dto and db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './modules/user/user.controller';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule , ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController, UserController],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+    ConfigModule.forRoot(),
+    UserModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async create(username: string, name: string, password: string): Promise<User> {
+    const user = new User();
+    user.username = username;
+    user.name = name;
+    user.password = password; // You should hash the password before saving it
+    return this.usersRepository.save(user);
+  }
+}
