87279d9cbd19ee4da90837f9e81e71688c592cbb
Priyankara R <priyankarar@opusxenta.com>
2023-08-08 18:30:58 +0530

    users table name changed

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
+  private readonly logger = new Logger(UserService.name);
+
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
@@ -15,6 +17,9 @@ export class UserService {
     user.username = username;
     user.name = name;
     user.password = password; // You should hash the password before saving it
-    return this.usersRepository.save(user);
+
+    const savedUser = this.usersRepository.save(user);
+    this.logger.log('User ${savedUser.id} created');
+    return savedUser;
   }
 }
